4ff3187bebc1190ea0e06085a65c28329d18078b
oliver <oliver.l.carrillo@gmail.com>
2024-01-15 15:47:06 -0600

    feat: automation  - loglevel

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -2,7 +2,7 @@ import logging
 import os
 import sys
 
-default_log_level = logging.DEBUG
+default_log_level = logging.ERROR
 
 
 def setup_logger(name: str, log_file: str, level: int = default_log_level):
